5237e6297b9e08a06851748d86e0c2312c117796
Cleiton <cleitonotaviano1980@hotmail.com>
2017-06-27 18:09:49 -0300

    Commit Inicial

---
+++ src/br/com/mecanica/domain/Fabricante.java
@@ -0,0 +1,19 @@
+package br.com.mecanica.domain;
+
+public class Fabricante {
+	
+	private Long codigo;
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	private String descricao;
+}
